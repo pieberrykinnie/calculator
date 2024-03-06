a06eb15aac1aaa7206e19079de66ba3af10fa285
Peter Vu <dmtruong1980@gmail.com>
2024-03-06 16:51:36 -0600

    feat: Create operate function

--- script.js
+++ script.js
@@ -4,3 +4,35 @@ multiply = (num1, num2) => num1 * num2;
 divide = (num1, num2) => num1 / num2;
 
 let num1, num2, operation;
+
+function operate(operation, num1, num2) {
+    const ADD = "+";
+    const SUBTRACT = "-";
+    const MULTIPLY = "*";
+    const DIVIDE = "/";
+
+    let result;
+
+    switch (operation) {
+        case ADD:
+            result = add(num1, num2);
+            break;
+
+        case SUBTRACT:
+            result = subtract(num1, num2);
+            break;
+
+        case MULTIPLY:
+            result = multiply(num1, num2);
+            break;
+
+        case DIVIDE:
+            result = divide(num1, num2);
+            break;
+
+        default:
+            break;
+    }
+
+    return result;
+}
